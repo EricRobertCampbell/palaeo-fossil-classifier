3780cecba737ab59984a155757c550cc0c55da08
Eric Campbell <eric.robert.campbell@gmail.com>
2024-06-15 08:38:42 -0600

    chore: added semantic versioning and linting on pr / push into main
    Closes #2

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -4,10 +4,6 @@ import { RuleConfigSeverity } from '@commitlint/types'
 const Configuration: UserConfig = {
     extends: ['@commitlint/config-conventional'],
     formatter: '@commitlint/format',
-    rules: {
-        'type-enum': [RuleConfigSeverity.Error, 'always', ['foo']],
-    },
-    // ...
 }
 
 export default Configuration
