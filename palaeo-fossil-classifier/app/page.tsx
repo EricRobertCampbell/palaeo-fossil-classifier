0edb68afdce31dcce9df6a01c887a3806b22c7d0
rahulnag7963 <rahulnag7963@gmail.com>
2024-12-15 19:18:51 -0700

    Made lots of changes to ui
    Revert "Made lots of changes to ui"
    
    This reverts commit 3bf8cb89df4f57a6c54b5f6d6f6b6fb481139595.
    
    Updated frontend ui

--- palaeo-fossil-classifier/app/page.tsx
+++ palaeo-fossil-classifier/app/page.tsx
@@ -1,16 +1,33 @@
 "use client";
-
-import Image from "next/image";
 import styles from "./page.module.css";
-
+import Link from "next/link";
+import useEmblaCarousel from "embla-carousel-react";
 export default function Home() {
+  const [emblaRef] = useEmblaCarousel();
   return (
-    <div className={styles.page}>
-      <header className={styles.header}> Palaeo Fossil Classifier </header>
-      <main className={styles.main}>
-        <p>Welcome to the Palaeo Fossil Classifier!</p>
-      </main>
-      <footer className={styles.footer}></footer>
+    <div>
+      <div className={styles.hero}>
+        <div className={styles.page}>
+          <header className={styles.header}> Palaeo Fossil Classifier </header>
+
+          <main className={styles.main}>
+            <div className={styles.grid}>
+              <p className={styles.content}>
+                Helping experts identifying fossils
+              </p>
+            </div>
+            <div className={styles.buttons}>
+              <Link href="./login">
+                <button className={`${styles.button} ${styles.type1}`}>
+                  <span className={styles.btntxt}>Enter</span>
+                </button>{" "}
+              </Link>
+            </div>
+          </main>
+        </div>
+
+        <footer className={styles.footer}></footer>
+      </div>
     </div>
   );
 }
